e425c480b9c6406d264a0d1909e1e9e67b2dc685
Sef Kloninger <sef@kloninger.com>
2013-06-08 01:28:13 -0700

    one-off script for setting video metadata (content-disposition)
    You need to set the content-disposition:attachment metadata to force the
    object to download.  This little boto one-off accomplishes that.
    
    Would be much nicer if this got the prior metadata and then only changed
    the value that we wanted to change, but I don't need that right now, and
    I'd rather sleep.

---
+++ transcoding/set-s3-metadata.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import boto
+
+conn = boto.connect_s3()
+bucket = conn.get_bucket('prod-edx')
+
+search = 'MedStats/Video240p/'
+
+for k in bucket.list(prefix=search):
+    if k.name == search:
+        # don't fiddle with the "directory" itself, just its contents
+        continue
+
+    print k
+    metadata = {'Content-Type': 'video/mp4',
+            'Content-Disposition': 'attachment',
+            }
+
+    # trick: copy a key to itself can change metadata
+    k = k.copy(k.bucket.name, k.name, metadata, preserve_acl=True)
+
