b48cea2525f0ae32a31b7902f26f9ec54e4a1f1b
Sef Kloninger <sef@kloninger.com>
2013-06-15 09:48:37 -0700

    start-transcode tool: parameters and comments
    Ideally this would take sane command line options, but too lazy to do
    that now.  In the interim just added lots of helpful comments.

--- transcoding/start-transcodes.py
+++ transcoding/start-transcodes.py
@@ -1,17 +1,62 @@
 #!/usr/bin/env python
+#
+# This relies on a pretty current boto, as elastictranscoder support 
+# was just added.  I use v.2.9.5.
+# 
+# Assumes that you have credentials to access AWS via boto, see:
+# http://boto.readthedocs.org/en/latest/boto_config_tut.html
+#
+# Doesn't explicity rely on the aws command line tools, but they are 
+# referenceed here, get them at http://aws.amazon.com/cli/
 
 import boto.elastictranscoder 
 import re
 
-transcoder = boto.elastictranscoder.connect_to_region('us-west-1')
-
+# 
+# SETTINGS
+#
+# Pipeline -- use this command to get the show the pipelines, use the ID:
+#     aws elastictranscoder list-pipelines
 pipeline_id = "1370295917356-5w004x"
-preset_id = "1351620000001-000060"   # 320x240 aka 240p
 
+# Output Format
+# Uncomment the one you want.  There are others available or you can create 
+# your own, get the ID from this command:
+#     aws elastictanscoder list-presets
+# preset_id = "1351620000001-000001"   # Generic 1080p
+preset_id = "1351620000001-000010"   # Generic 720p
+# preset_id = "1351620000001-000020"   # Generic 480p 16:9
+# preset_id = "1351620000001-000030"   # Generic 480p 4:3
+# preset_id = "1351620000001-000040"   # Generic 360p 16:9
+# preset_id = "1351620000001-000050"   # Generic 360p 16:9
+# preset_id = "1351620000001-000060"   # Generic 320x240
+
+# Region
+reg='us-west-1'
+
+# List of videos to encode
+# can be an explicit list with entries that look like this (case sensitive, 
+# no leading slash):
+#      MedStats/VideoLarge/Unit 8 Module 5.mp4
+# command I use to generate the list:
+#      aws s3 list-objects --bucket=prod-edx --output=text --prefix="MedStats/VideoLarge" | cut -f4 | grep -v ^$
 filelist = open("/Users/sef/medstats-videos.txt")
+
+# Path Substitution
+# Folder to rename from to rename to
+from_dir = 'VideoLarge'
+to_dir = 'Video720p'
+
+
+#
+# ACTUALLY DO THE TRANSCODES
+# 
+
+transcoder = boto.elastictranscoder.connect_to_region(reg)
+
 for f in filelist:
     in_key = f.strip()
-    out_key = re.sub('VideoLarge', 'Video240p', in_key)
+    out_key = re.sub(from_dir, to_dir, in_key)
 
     params_in = { 'Key':    in_key,
         'FrameRate':        'auto',
