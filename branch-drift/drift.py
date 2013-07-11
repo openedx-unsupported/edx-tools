4f4b58373003d321a9cfb1f60d99b6602edd7fa5
Sef Kloninger <sef@kloninger.com>
2013-07-11 19:10:07 -0400

    drift: add parameters; calc dates from git branches

--- branch-drift/drift.py
+++ branch-drift/drift.py
@@ -1,22 +1,31 @@
 #!/usr/bin/env python
 
-import matplotlib.pyplot as plt
 import os
 import datetime
+from dateutil import parser
 import subprocess
 import re
+from optparse import OptionParser, OptionError
 from collections import OrderedDict
+import matplotlib.pyplot as plt
 
+DEFAULT_REPO_DIR = "../../edx-platform"
+DEFAULT_FROM_BRANCH = "edx-west/release"
+DEFAULT_TO_BRANCH = "master"
+DEFAULT_DIFF_FILENAME = "diff"
 
 def main():
-    os.chdir("../../edx-platform")
+    options = parsecommandline()
+    os.chdir(options.repodir)
 
-    start_date = datetime.date(2013,06,01)
-    end_date = datetime.date.today()
-    from_branch = "edx-west/release"
+    from_branch = options.frombranch
     from_branch_origin = "origin/"+from_branch
-    to_branch = "master"
+    to_branch = options.tobranch
     to_branch_origin = "origin/"+to_branch
+
+    (start_date, end_date) = calculate_dates(options, from_branch_origin, to_branch_origin)
+    print "scanning from %s to %s" % (start_date.strftime("%Y-%m-%d"), \
+                end_date.strftime("%Y-%m-%d"))
 
     (difflines, diffblocks) = branch_diffs(from_branch_origin, to_branch_origin, 
             start_date, end_date)
@@ -31,6 +40,76 @@ def main():
     plt.plot(diffblocks.keys(), diffblocks.values())
 
     plt.savefig("diff")
+
+
+def parsecommandline():
+    usage = """usage: %prog [options]
+
+Generate graphs comparing branches in an edx repo checked out closeby."""
+
+    parser = OptionParser(usage=usage)
+
+    parser.add_option("-d", dest="repodir", default=DEFAULT_REPO_DIR,
+            help="relative path to the edx-platform repo " + \
+                    "(default \"%s\")" % DEFAULT_REPO_DIR)
+    parser.add_option("-f", dest="frombranch", default=DEFAULT_FROM_BRANCH,
+            help="branch comparing from, do not include \"origin\" " + \
+                    "(default \"%s\")" % DEFAULT_FROM_BRANCH)
+    parser.add_option("-t", dest="tobranch", default=DEFAULT_TO_BRANCH,
+            help="branch comparing to, do not include \"origin\" " + \
+                    "(default \"%s\")" % DEFAULT_TO_BRANCH)
+    parser.add_option("-s", dest="startdate", default=None,
+            help="date to begin with, default is oldest date common to both branches." + \
+                    "Use this to force an older date (ie past rebases).")
+    parser.add_option("-e", dest="enddate", default=None,
+            help="date to end with (default today)")
+    parser.add_option("-g", dest="diff_filename", default=DEFAULT_DIFF_FILENAME,
+            help="file for diff graph relative to repo dir " +\
+                    "(default \"%s\")" % DEFAULT_DIFF_FILENAME)
+
+    options, args = parser.parse_args()
+    if args:
+        raise OptionError("unknown option")
+
+    return options
+
+
+def calculate_dates(options, from_branch_origin, to_branch_origin):
+    """
+    Either parse date strings given on the command line, or figure out defaults.
+    end is easy (today), start means looking at branches.
+    """
+    if options.startdate:
+        start_date = parser.parse(options.startdate).date()
+    else:
+        fromdate = beginning_of_branch(from_branch_origin)
+        todate = beginning_of_branch(to_branch_origin)
+        start_date = min(fromdate,todate).date()
+
+    if options.enddate:
+        end_date = parser.parse(options.enddate).date()
+    else:
+        end_date = datetime.date.today()
+
+    return (start_date, end_date)
+
+
+def beginning_of_branch(name):
+    commit = oldest_commit_on_branch(name)
+    date = date_from_git_commithash(commit)
+    print "oldestcommit on", name, "is", commit, " date =", date.strftime("%Y-%m-%d")
+    return date
+
+def oldest_commit_on_branch(branchname):
+    gitcmd = ['git', 'rev-list', '--first-parent', '-1', branchname]
+    hashstr = subprocess.check_output(gitcmd)
+    return hashstr.strip()
+
+def date_from_git_commithash(commithash):
+    # Consider %ad instead for the author date. This is committer date.
+    gitcmd = ['git', 'show', '-s', '--format=%ci', commithash]
+    datestr = subprocess.check_output(gitcmd)
+    return parser.parse(datestr)
 
 
 def branch_diffs(from_branch, to_branch, start_date, end_date):
@@ -66,3 +145,4 @@ def branch_diffs(from_branch, to_branch, start_date, end_date):
 
 if __name__ == '__main__':
     main()
+
