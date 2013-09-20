2413662e88198aa47dd577773b59b125396fe9c0
Sef Kloninger <sef@kloninger.com>
2013-09-20 16:36:18 -0700

    add -n option for dry run

--- transcoding/set-s3-metadata.py
+++ transcoding/set-s3-metadata.py
@@ -3,11 +3,21 @@
 import sys
 import boto
 
+dry_run = False
+try:
+    optpos = sys.argv.index('-n')
+    dry_run = True
+    sys.argv.pop(optpos)  # modify argv
+except ValueError:
+    pass
+
 if len(sys.argv) == 3:
     bucket_name = sys.argv[1]
     search = sys.argv[2]
 else:
-    print "usage", sys.argv[0], "bucket path"
+    print "usage", sys.argv[0], "[-n] bucket path"
+    print "    -n for dry-run"
+    print
     print "    example:", sys.argv[0], "prod-edx SciWrite/VideoLarge/"
     sys.exit(1)
 
@@ -19,11 +29,14 @@ for k in bucket.list(prefix=search):
         # don't fiddle with the "directory" itself, just its contents
         continue
 
-    print k
     metadata = {'Content-Type': 'video/mp4',
             'Content-Disposition': 'attachment',
             }
 
-    # trick: copy a key to itself can change metadata
-    k = k.copy(k.bucket.name, k.name, metadata, preserve_acl=True)
+    if dry_run:
+        print "skip:", k
+    else:
+        print "fix:", k
+        # trick: copy a key to itself can change metadata
+        k = k.copy(k.bucket.name, k.name, metadata, preserve_acl=True)
 
