2c4c4ba58c531b456fb5bb3a3b7c8f94f5f1749c
Luis Moreno <luis.moreno@edunext.co>
2020-07-02 16:32:12 -0400

    Modernize lti and ip2country

--- ip2country/ip2country.py
+++ ip2country/ip2country.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+from __future__ import absolute_import
+from __future__ import print_function
 import fileinput
 import collections
 import pygeoip
@@ -10,5 +12,5 @@ for ip in fileinput.input():
     country = lookup.country_code_by_addr(ip)
     totals[country] += 1
 for country in sorted(totals, key=totals.get, reverse=True):
-    print country+","+str(totals[country])
+    print(country+","+str(totals[country]))
 
